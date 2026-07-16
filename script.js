eb474f54789ad0fd1bcce5348746bdf06123bb28
TaylorTisdall <tayloretisdall@gmail.com>
2026-07-15 20:42:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Writing Lab scheduler connected!");
+const SUPABASE_URL = "https://mdnyzlzaarzozbmqhecz.supabase.co/rest/v1/";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kbnl6bHphYXJ6b3pibXFoZWN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxNTU5NzgsImV4cCI6MjA5OTczMTk3OH0.FYJgBDOhmqg516ApoPAqWezMIaSIHseoKCcUcU6m-To";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+console.log("Supabase connected!");
